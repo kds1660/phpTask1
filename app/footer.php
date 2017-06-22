904a2e5abbd5ae2c3a5504cc73d76d8c4e52f289
Kds <kds16061985@gmail.com>
2017-06-22 21:19:50 +0300

    change footer

--- app/footer.php
+++ app/footer.php
@@ -1,5 +1,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="public/js/bootstrap.min.js"></script>
-<footer class="footer">
-    <span>2017</span>
+<footer class="footer navbar-fixed-bottom">
+    <span>2017 year</span>
 </footer>
