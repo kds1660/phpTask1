3cf0fd37bc6e0030bd8edd9df4d43bb0ec7ee1a6
Kds <kds16061985@gmail.com>
2017-06-22 22:31:44 +0300

    change requests

--- app/include/requestText2.php
+++ app/include/requestText2.php
@@ -8,11 +8,11 @@ where studios.name="$studio"
 GROUP by actors.id_actor
 sql1;
 $sql[] = <<<sql1
-SELECT studios.name as StudioName, count(DISTINCT flm.id_film) as films, count(DISTINCT fs.id_fee) as fees_number, sum(fs.fee) as fees_sum, avg(fs.fee) as fees_avg from studios
+SELECT studios.name as StudioName, count(DISTINCT flm.id_film) as films, count(fs.id_fee) as fees_number, sum(fs.fee) as fees_sum, avg(fs.fee) as fees_avg from studios
 inner join studio_films sf USING (id_studio)
 inner join films flm on (flm.id_film=sf.id_film and flm.year>adddate(curdate(), interval -10 year))
 inner join fees fs on (fs.id_film=flm.id_film)
 where
 studios.name="$studio"
-GROUP by StudioName
+GROUP by studios.id_studio
 sql1;
